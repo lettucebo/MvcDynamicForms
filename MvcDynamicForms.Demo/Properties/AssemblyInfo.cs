f238a7ae201b71f8995cbfde47630b55efae0dc5
Money <abc12207@gmail.com>
2016-03-28 07:19:14 +0800

    fix: demo is broken

--- MvcDynamicForms.Demo/Properties/AssemblyInfo.cs
+++ MvcDynamicForms.Demo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Creatidea.Library.Web.DynamicForms.Demo")]
+[assembly: AssemblyTitle("MvcDynamicForms.Demo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Creatidea.Library.Web.DynamicForms.Demo")]
+[assembly: AssemblyProduct("MvcDynamicForms.Demo")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
